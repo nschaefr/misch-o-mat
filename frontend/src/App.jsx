ee40e01c6855c20ad47aa49f5b6df43b3bb698e5
nschaefr <nils.schaefer@tuta.io>
2025-01-31 12:19:02 +0100

    update frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,15 @@
-import { Routes, Route } from 'react-router-dom';
-import Customer from './pages/Customer'
-import Preparation from './pages/Preparation'
-import Pin from './pages/Pin';
-import Ready from './pages/Ready';
-import Settings from './pages/Settings';
-import Configuration from './pages/Configuration';
+import { Routes, Route } from "react-router-dom";
+import Customer from "./pages/Customer";
+import Preparation from "./pages/Preparation";
+import Pin from "./pages/Pin";
+import Ready from "./pages/Ready";
+import Settings from "./pages/Settings";
+import Configuration from "./pages/Configuration";
+import Calibration from "./pages/Calibration";
 
 function App() {
-
   return (
-    <div className='px-4 w-full h-full'>
+    <div className="px-4 w-full h-full">
       <Routes>
         <Route path="/" element={<Customer />} />
         <Route path="/preparation" element={<Preparation />} />
@@ -17,9 +17,10 @@ function App() {
         <Route path="/ready" element={<Ready />} />
         <Route path="/settings" element={<Settings />} />
         <Route path="/configuration" element={<Configuration />} />
+        <Route path="/calibration" element={<Calibration />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
